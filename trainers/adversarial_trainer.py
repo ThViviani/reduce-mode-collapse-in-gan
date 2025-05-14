3a91fd9fa02a1267a246b0a2341e222cfde9d9b1
@ThViviani <developsviviani@gmail.com>
2025-05-14 15:45:19 +1000

    add class for a adversarial training process

---
+++ trainers/adversarial_trainer.py
@@ -0,0 +1,99 @@
+import lightning as L
+import torch
+import torch.nn as nn
+import torchvision.utils as vutils
+import numpy as np
+
+from utils.train_options import TrainOptions
+
+
+class AdversarialTraining(L.LightningModule):
+    def __init__(
+            self, 
+            critic: nn.Module, 
+            generator: nn.Module, 
+            opt: TrainOptions,
+    ):
+        super().__init__()
+        self.automatic_optimization = False
+        self.critic = critic
+        self.generator = generator
+        
+        self.critic.apply(self._initialize_weights)
+        self.generator.apply(self._initialize_weights)
+
+        self.opt = opt 
+        self.fixed_latents = torch.randn(opt.batch_size, opt.latent_dim, 1, 1)
+
+    def _initialize_weights(self, m):
+        if isinstance(m, (nn.Conv2d)):
+            nn.init.normal_(m.weight.data, 0, 0.02)
+        elif isinstance(m, (nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+
+    def configure_optimizers(self):
+        crititc_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.opt.lr, betas=self.opt.betas)
+        generator_optimizer = torch.optim.Adam(self.generator.parameters(), lr=self.opt.lr, betas=self.opt.betas)
+        return [crititc_optimizer, generator_optimizer]
+    
+    def __log_images(self, n_generated_imgs: int = 32):    
+        generated_imgs = self.generator(self.fixed_latents.to(self.device))
+        
+        # Convert to proper format and normalize
+        grid = vutils.make_grid(generated_imgs[:n_generated_imgs], padding=2, normalize=True).cpu()
+        
+        self.logger.log_image(
+            key="generated_progress",
+            images=[grid],
+            caption=[f"epoch {self.current_epoch}"],
+            step=self.current_epoch
+        )
+    
+    def on_train_epoch_end(self):
+        self.__log_images()
+
+    def __log_gradients(self):
+        g_grad = [p.grad.view(-1).cpu().numpy() for p in list(self.generator.parameters())]
+        d_grad = [p.grad.view(-1).cpu().numpy() for p in list(self.critic.parameters())]
+        
+        grad_history = {
+            'g_grads_mean': np.concatenate(g_grad).mean().item(),
+            'g_grads_std': np.concatenate(g_grad).std().item(),
+            'd_grads_mean': np.concatenate(d_grad).mean(),
+            'd_grads_std': np.concatenate(d_grad).std()
+        }
+        self.log_dict(grad_history, prog_bar=True)
+
+    def _critic_loss(self, x_real: torch.Tensor, criterion: nn.Module) -> torch.Tensor:
+        raise NotImplementedError
+
+    def _generator_loss(self, x_real: torch.Tensor, criterion: nn.Module) -> torch.Tensor:
+        raise NotImplementedError
+
+    def training_step(self, batch, batch_idx):
+        critic_optimizer, generator_optimizer = self.optimizers()
+        x_real, _ = batch
+        criterion = nn.BCEWithLogitsLoss() 
+
+        # Critic's train
+        for _ in range(self.opt.n_critic_steps):
+            critic_optimizer.zero_grad()
+            critic_loss = self._critic_loss(x_real, criterion)
+            self.manual_backward(critic_loss)
+            critic_optimizer.step() 
+
+        # Generator's train
+        generator_optimizer.zero_grad()
+        gen_loss = self._generator_loss(x_real, criterion)
+        self.manual_backward(gen_loss)
+        generator_optimizer.step()
+
+        history = { 
+            'loss_g': gen_loss.item(), 
+            'loss_d': critic_loss.item(),
+        }
+        
+        self.log_dict(history, prog_bar=True)
+        self.__log_gradients()
+        return history  
---
+++ trainers/standard_gan.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+from trainers.adversarial_trainer import AdversarialTraining
+
+
+class StandardGAN(AdversarialTraining):
+    def _critic_loss(self, x_real: torch.Tensor, criterion: nn.Module) -> torch.Tensor:
+        critic_real = self.critic(x_real)
+        real_err = criterion(critic_real, torch.ones_like(critic_real, device=self.device))
+        
+        z = torch.randn(x_real.size(0), self.opt.latent_dim, device=self.device)
+        x_fake = self.generator(z)
+        critic_fake = self.critic(x_fake.detach())
+        fake_err = criterion(critic_fake, torch.zeros_like(critic_fake, device=self.device))
+        return real_err + fake_err      
+        
+    def _generator_loss(self, x_real: torch.Tensor, criterion: nn.Module) -> torch.Tensor:
+        z = torch.randn(x_real.size(0), self.opt.latent_dim, device=self.device)
+        x_fake = self.generator(z)
+        critic_fake = self.critic(x_fake)
+        gen_loss = criterion(critic_fake, torch.ones_like(critic_fake, device=self.device))
+        return gen_loss
