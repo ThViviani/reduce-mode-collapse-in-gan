c87b56d9c646bac21945ac95502af609a56e6a9d
@ThViviani <developsviviani@gmail.com>
2025-05-14 12:55:33 +1000

    add initial train options

---
+++ utils/train_options.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class TrainOptions:
+    """Defines a train option for models"""
+    
+    latent_dim: int = 100
+    batch_size: int = 256
+    lr: float = 2e-4
+    betas: tuple = (0.5, 0.999)
+    n_critic_steps: int = 1
+    use_gp: bool = False
+    gamma_gp: float = 1.
