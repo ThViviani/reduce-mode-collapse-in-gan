df9987cc795e77ba8bc28a7c57b837d1ed6c04b3
@ThViviani <developsviviani@gmail.com>
2025-05-23 19:54:14 +1000

    add DpVanilaGAN, DpRpGAN train classes

--- trainers/dp_gan.py
+++ trainers/dp_gan.py
@@ -34,7 +34,7 @@ class DpVanilaGan(DiversityPenaltyMixin, StandardGAN):
     def _generator_loss(self, x_real: torch.Tensor, criterion: nn.Module) -> torch.Tensor:
         base_loss = super()._generator_loss(x_real, criterion)
         lambda_dp = self.opt.diversity_penalty
-        dp_loss = self._diversity_penalty_loss(x_real, self._compute_scale_for_dp())
+        dp_loss = self._diversity_penalty_loss(x_real.size(0), self._compute_scale_for_dp())
         g_loss = base_loss + lambda_dp * dp_loss
         self.log_dict({'dp_loss': dp_loss}, prog_bar=True)  
         return g_loss
