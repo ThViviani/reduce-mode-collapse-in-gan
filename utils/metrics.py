6eda4f27ab513100b0cee5d2b5fdc6099ce73a40
@ThViviani <developsviviani@gmail.com>
2025-06-18 18:52:38 +1000

    add a modes covered metric callback for trainers

---
+++ utils/metrics.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+import torch
+import lightning as L
+
+from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.loggers import WandbLogger
+
+
+class ModesCovered(Callback):
+    def __init__(
+        self,
+        classifier: nn.Module,
+        z_dim: int,
+        total_samples: int = 25600,
+        batch_size: int = 256,
+        confidence=0.99,
+        n_classes=1000,
+    ):
+        super().__init__()
+        self.classifier = classifier.eval()
+        self.z_dim = z_dim
+        self.total_samples = total_samples
+        self.batch_size = batch_size
+        self.confidence = confidence
+        self.n_classes = n_classes
+
+    def _compute_modes_covered(self, preds_labels, n_classes=1000):
+        modes_covered = torch.bincount(preds_labels.int(), minlength=n_classes)
+        return (modes_covered > 0).sum().item()
+
+    def on_train_epoch_end(self, trainer: L.Trainer, pl_module: L.LightningModule) -> None:
+        self.classifier.to(pl_module.device)
+        num_batches = self.total_samples // self.batch_size
+        preds_labels = []
+        for _ in range(num_batches):
+            z = torch.randn(self.batch_size, self.z_dim, device=pl_module.device)
+            with torch.no_grad():
+                fake_imgs = pl_module.generator(z)
+                logits    = self.classifier(fake_imgs)
+                p_logits = torch.nn.functional.softmax(logits).detach()
+                confidences, labels_hat = torch.max(p_logits, dim=1)
+                preds_labels.extend(labels_hat * (confidences > self.confidence))
+
+        preds_labels = torch.Tensor(preds_labels)
+        modes_covered = self._compute_modes_covered(preds_labels, self.n_classes)
+        
+        if isinstance(trainer.logger, WandbLogger):
+            wandb_run = trainer.logger.experiment 
+            
+            wandb_run.log(
+                {"modes_covered": modes_covered},
+                step=trainer.global_step,
+            )
